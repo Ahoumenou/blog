f045d4de78690fb138b7a9087fbc693a4e100aeb
Ahoumenou Emile <ahoumenouemile022@gmail.com>
2023-09-13 19:25:52 +0100

    niveau-delete

--- resources/views/partials/article.blade.php
+++ resources/views/partials/article.blade.php
@@ -1,5 +1,5 @@
 <article>
-    <h3>Article écrit par: <strong> <em> {{ $article->user->name }}</em> </strong> </h3> 
+    {{-- <h3>Article écrit par: <strong> <em> {{ $article->user->name }}</em> </strong> </h3>  --}}
         <p>Titre: <a href="/article/{{ $article->title }}">{{ $article['title'] }} </a></p>   
     <a href="/article/{{ $article->id}}">Détails</a>
     {{-- <a href="/article/{{ $article->id }}">Article</a> --}}
